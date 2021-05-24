c071920edce2118cb28a3e8641c4f52b2b5fc075
kirti <sawantkriti4@gmail.com>
2021-05-24 19:03:20 +0530

    Required

---
+++ src/main/java/com/CoreSpring/Annotation/Company.java
@@ -0,0 +1,32 @@
+package com.CoreSpring.Annotation;
+
+import org.springframework.beans.factory.annotation.Required;
+
+public class Company {
+    private Integer cid;
+    private String cname;
+
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    @Required
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    @Required
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String toString() {
+        return "Company [cid=" + cid + "cname=" + cname + "]";
+
+    }
+}
---
+++ src/main/java/com/CoreSpring/Annotation/Test.java
@@ -0,0 +1,13 @@
+package com.CoreSpring.Annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Company.xml");
+        //  context.registerShutdownHook();
+        Company company = (Company) context.getBean("company");
+        System.out.println(company);
+    }
+}
